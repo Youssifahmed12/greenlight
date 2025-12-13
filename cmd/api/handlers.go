86f6ed203833653f2bdfc643b86ff4b8d61ea25a
Youssifahmed12 <youssefbro12397@gmail.com>
2025-12-14 01:29:11 +0200

    layout and some basic handlers

---
+++ cmd/api/handlers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "movie with id %d", id)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Youssif12/greenlight
+
+go 1.25.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
