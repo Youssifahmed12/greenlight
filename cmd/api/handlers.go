9437b8ee77c58417a8e9559efa911f22cb864872
Youssifahmed12 <youssefbro12397@gmail.com>
2025-12-15 01:29:54 +0200

    some ground work

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Youssif12/greenlight/internal/data"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte(js))
+	err := app.writeJson(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,5 +36,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "movie with id %d", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
