adc009c4f71537c7355e433f5e469cd79843a499
Youssifahmed12 <Youssefbro12397@gmail.com>
2025-12-15 14:02:41 +0200

    ALOT of error handling

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int
 
@@ -13,4 +17,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJsonValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJsonValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSON, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSON, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
 }
